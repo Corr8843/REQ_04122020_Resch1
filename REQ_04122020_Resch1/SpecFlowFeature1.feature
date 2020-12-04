9aa986cc46baaee6e3bf9ea8cef8801c1f314de6
BP <wi19b024@technikum-wien.at>
2020-12-04 14:53:12 +0100

    Add project files.

---
+++ REQ_04122020_Resch1/SpecFlowFeature1.feature
@@ -0,0 +1,29 @@
+﻿Feature: SpecFlowFeature1
+    In order to avoid silly mistakes
+    As a math idiot
+    I want to add, subtract, multiply and divide
+
+@mytag
+Scenario: Add two numbers
+    Given the first number is 70
+    And the second number ist 50
+    When the two numbers are added
+    Then the result should be 120
+
+Scenario: Subtract two numbers
+    Given the first number is 70
+    And the second number ist 50
+    When the two numbers are subtracted
+    Then the result should be 20
+
+Scenario: Multiply two numbers
+    Given the first number is 5
+    And the second number ist 7
+    When the two numbers are multiplied
+    Then the result should be 35
+
+Scenario: Divide two numbers
+    Given the first number is 70
+    And the second number ist 10
+    When the first number is divided by the second number
+    Then the result should be 7
---
+++ REQ_04122020_Resch1/SpecFlowFeature1Steps.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TechTalk.SpecFlow;
+using Xunit;
+
+namespace RQE_04122020
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        double v1;
+        double v2;
+        double res;
+
+        [Given(@"the first number is (.*)")]
+        public void GivenTheFirstNumberIs(int p0)
+        {
+            v1 = p0;
+        }
+
+        [Given(@"the second number ist (.*)")]
+        public void GivenTheSecondNumberIst(int p0)
+        {
+            v2 = p0;
+        }
+
+        [When(@"the two numbers are added")]
+        public void WhenTheTwoNumbersAreAdded()
+        {
+            res = v1 + v2;
+        }
+
+        [When(@"the two numbers are subtracted")]
+        public void WhenTheTwoNumbersAreSubtracted()
+        {
+            res = v1 - v2;
+        }
+
+        [When(@"the two numbers are multiplied")]
+        public void WhenTheTwoNumbersAreMultiplied()
+        {
+            res = v1 * v2;
+        }
+
+        [When(@"the first number is divided by the second number")]
+        public void WhenTheFirstNumberIsDividedByTheSecondNumber()
+        {
+            res = v1 / v2;
+        }
+
+        [Then(@"the result should be (.*)")]
+        public void ThenTheResultShouldBe(int p0)
+        {
+            Assert.Equal(p0, res, 5);
+        }
+
+    }
+}
